77001283ece5c6a24528d46a0ecaa89b84aaaee8
Nick Bernini <nabernini@gmail.com>
2018-12-08 10:54:17 -0500

    update files for training and predicting dad face recognition

--- config.py
+++ config.py
@@ -44,7 +44,7 @@ DEBUG_IMAGE = 'capture.jpg'
 # Start with a value of 3000, but you might need to tweak this value down if 
 # you're getting too many false positives (incorrectly recognized faces), or up
 # if too many false negatives (undetected faces).
-POSITIVE_THRESHOLD = 2000.0
+POSITIVE_THRESHOLD = 3000.0
 
 # File to save and load face recognizer model.
 TRAINING_FILE = 'training.xml'
